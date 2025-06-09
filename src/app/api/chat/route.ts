c7ac1be8d1c541bbc4e195165cf48f70621096c8
Dante Dupont <dmxdupont@gmail.com>
2025-06-09 11:40:19 -0400

    zod time

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,11 +1,24 @@
 import { xai } from '@ai-sdk/xai';
-import { streamText } from 'ai';
+import { coreMessageSchema, streamText } from 'ai';
+import { parse } from 'path';
+import { z } from "zod"
 
 // Allow streaming responses up to 30 seconds
 export const maxDuration = 30;
 
+// Defining Zod schema
+const requestBodySchema = z.object({
+    messages: z.array(coreMessageSchema)
+})
+
+// Infer the TS type from schema
+type RequestBody = z.infer<typeof requestBodySchema>
+
 export async function POST(req: Request) {
-  const { messages } = await req.json();
+  let parsedBody: RequestBody
+  const requestJson = await req.json();
+  parsedBody = requestBodySchema.parse(requestJson)
+  const { messages } = parsedBody
 
   const result = streamText({
     model: xai('grok-3'),
